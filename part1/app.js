1332f3f5a012fa83a8cd9423cf00ad5ad733b89d
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 12:39:12 +0000

    [automatic commit] 2025-06-20 12:39:12 UTC 122.150.228.186

--- part1/app.js
+++ part1/app.js
@@ -12,5 +12,6 @@ const dbConfig = {
   let connection;
 
 async function seedDatabase() {
-    try{
-        const[users[0].count ===0] {
+    try
+    const [users] = await connection.query('SELECT COUNT(*) as count FROM Users');
+    if (users[0].count === 0) {
