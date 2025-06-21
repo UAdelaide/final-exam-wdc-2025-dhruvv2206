f357411a6feabdf6d150d6960f0b9df4edaa936c
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 04:01:16 +0000

    [automatic commit] 2025-06-21 04:01:16 UTC 129.127.202.127

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const session = rewuire
+const session = require ('express-session');
+
 require('dotenv').config();
 
 const app = express();
